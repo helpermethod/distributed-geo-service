3ed5f0a50e5ce4cd0e8229762860a2d55ba64958
helpermethod <weiler@predic8.de>
2016-09-09 18:30:03 +0200

    Final version

--- cloud-config-server/src/main/java/de/predic8/boot/CloudConfServerApplication.java
+++ cloud-config-server/src/main/java/de/predic8/boot/CloudConfServerApplication.java
@@ -2,10 +2,11 @@ package de.predic8.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class CloudConfServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CloudConfServerApplication.class, args);
 	}
