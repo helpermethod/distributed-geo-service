b62fbca0e0ccdd4bfd39adad2bfcd23b00f87293
helpermethod <weiler@predic8.de>
2016-09-09 11:54:07 +0200

    Register geo-service as Eureka instance

--- geo-service/src/main/java/de/predic8/cloud/GeoServiceApplication.java
+++ geo-service/src/main/java/de/predic8/cloud/GeoServiceApplication.java
@@ -2,8 +2,9 @@ package de.predic8.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GeoServiceApplication {
 	public static void main(String[] args) {
