6faa09784861e9a515843ed5ce9f438fb243dcc3
helpermethod <weiler@predic8.de>
2016-09-09 17:40:52 +0200

    Add missing files

---
+++ cloud-config-server/src/main/java/de/predic8/boot/CloudConfServerApplication.java
@@ -0,0 +1,12 @@
+package de.predic8.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfServerApplication.class, args);
+	}
+}
